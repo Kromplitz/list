a18ceb2565e1003c30a38aca2cbc7d8de911c88e
Kromplias <oles.media@gmail.com>
2024-02-14 19:59:47 +0200

    додав інтерфейс

--- app/src/main/java/com/example/list/ApiInterface.kt
+++ app/src/main/java/com/example/list/ApiInterface.kt
@@ -1,9 +1,15 @@
 package com.example.list
 
+import android.provider.MediaStore.Images
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-interface ApiInterface{
+//interface ApiInterface{
+  //  @GET("/superhero-api/api/all.json")
+   // fun getHeroes(): Single<List<Hero>>}
+
+interface ApiInterface {
     @GET("/superhero-api/api/all.json")
-    fun getHeroes(): Single<List<Hero>>
+    suspend fun getHeroes(@Path("all") name: String, images: String):Single<List<Hero>>
 }
