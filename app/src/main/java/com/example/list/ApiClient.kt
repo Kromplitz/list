2d13dfac1cdeb3fa6c2dec5f3dd3a4007f0c36e9
Kromplias <oles.media@gmail.com>
2024-02-14 20:20:39 +0200

    додав аплікешн

--- app/src/main/java/com/example/list/ApiClient.kt
+++ app/src/main/java/com/example/list/ApiClient.kt
@@ -7,12 +7,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 
-object ApiClient {
-    val client = Retrofit.Builder()
-        .client(OkHttpClient())
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .baseUrl("https://akabab.github.io")
-        .build()
-
-}
+//object ApiClient {
+   // val client = Retrofit.Builder()
+      //  .client(OkHttpClient())
+      //  .addConverterFactory(GsonConverterFactory.create())
+      //  .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+      //  .baseUrl("https://akabab.github.io")
+      //  .build() }
